cc1bb4991890b22ce72d78777f033113821667ed
my <jvckigh@gmail.com>
2022-02-05 03:23:01 +0900

    modern java in action, effective java 모듈 추가

---
+++ modern-java-in-action/src/test/java/study/modernjavainaction/ModernJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.modernjavainaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
